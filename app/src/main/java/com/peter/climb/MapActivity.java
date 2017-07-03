d450e85bfa1bf3d2d70816704b99b0a6da884d22
Peter Mitrano <mitranopeter@gmail.com>
2017-07-03 18:52:31 -0400

    got protobuf working
    working on map

--- app/src/main/java/com/peter/climb/MapActivity.java
+++ app/src/main/java/com/peter/climb/MapActivity.java
@@ -1,18 +1,27 @@
 package com.peter.climb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements View.OnClickListener {
 
     View decor_view;
+    ImageView map_image_view;
+    Button end_session_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         decor_view = getWindow().getDecorView();
+
+        end_session_button = (Button) findViewById(R.id.end_session_button);
+        end_session_button.setOnClickListener(this);
+
+        map_image_view = (ImageView) findViewById(R.id.map_image_view);
     }
 
     @Override
@@ -26,6 +35,14 @@ public class MapActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.end_session_button)
+        {
+            finish();
         }
     }
 }
