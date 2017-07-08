1c5ca5681ee6bc541f2c6cd363d3b3e9f783a075
Eli Skeggs <skeggse@users.noreply.github.com>
2017-07-08 15:02:43 -0700

    Support simple module registration

--- gym_builder/gym_builder/static/js/loader.js
+++ gym_builder/gym_builder/static/js/loader.js
@@ -1,3 +1,6 @@
+// Original work Copyright @frostney 2015
+// https://gist.github.com/frostney/9cdc735cc1e26487328e
+
 (function(root) {
 	
 	var modules = {};
@@ -13,7 +16,7 @@
 
 			modules[name].call(module, require, module, exports);
 
-			cache[name] = (Object.hasOwnProperty.call(module, 'exports')) ? module.exports : exports;
+			cache[name] = hasOwn.call(module, 'exports') ? module.exports : exports;
 		}
 
 		return cache[name];
@@ -21,7 +24,16 @@
 
 	// factory (require, module, exports)
 	root.require.register = function(name, factory) {
-		modules[name] = factory;
+		if (!factory) {
+			throw new TypeError('expected factory function or object');
+		}
+		if (typeof factory === 'object') {
+			modules[name] = function(require, module) {
+				module.exports = factory;
+			};
+		} else {
+			modules[name] = factory;
+		}
 	};
 
 })(window);
