cbad3f21dfe3cf01ff8d1196b08c0d343f1772b4
Peter Mitrano <mitranopeter@gmail.com>
2017-07-13 00:09:53 -0400

    gulp now generates protobuf
    follows symlink

--- gym_builder/gulpfile.js
+++ gym_builder/gulpfile.js
@@ -1,10 +1,17 @@
-let gulp = require('gulp');
-let browserify = require('gulp-browserify');
-let rename = require('gulp-rename');
+const gulp = require('gulp');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+const shell = require('gulp-shell');
 
 gulp.task('default', function() {
-  gulp.src(['./static/js/custom.js']).
+  gulp.src(['./js/custom.js']).
       pipe(browserify()).
       pipe(rename('bundle.js')).
-      pipe(gulp.dest('./static/js/'));
+      pipe(gulp.dest('./js'));
+});
+
+gulp.task('protoc', () => {
+  return gulp.src('').pipe(shell([
+    '/opt/protoc-3.0/bin/protoc --js_out=import_style=commonjs,binary:js/ proto/Gym.proto',
+  ]));
 });
