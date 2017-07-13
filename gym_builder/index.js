b6efb22621e03664e671d92507ecd75a2e906d7c
Peter Mitrano <mitranopeter@gmail.com>
2017-07-13 00:17:35 -0400

    remove python fully, and rename gyms
    because express static is set up, this works for free

--- gym_builder/index.js
+++ gym_builder/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const proto = require('google-protobuf');
 const msgs = require('./js/Gym_pb.js');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use("/", express.static(__dirname));
-
-app.get('/gyms', (request, response) => {
-  response.send('gyms');
-})
 
 app.listen(port, (err) => {
   if (err) {
