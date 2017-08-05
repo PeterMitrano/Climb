68a2342f6854dd1f5f159b9afe86696920e4d3f5
Peter Mitrano <mitranopeter@gmail.com>
2017-08-05 13:55:11 -0400

    filter for RightAlignedHintEdit

--- climb/app/src/main/java/com/peter/climb/Views/RightAlignedHintEdit.java
+++ climb/app/src/main/java/com/peter/climb/Views/RightAlignedHintEdit.java
@@ -6,6 +6,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
@@ -17,21 +19,19 @@ import com.peter.climb.R;
 public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeListener {
 
   private static final float UNDERBAR_HEIGHT = 4;
+
+  private float maxValue;
   private String inputType;
   private EditText edit;
   private TextView hint;
   private int unfocusColor;
   private int focusColor;
-
   private int textColor;
-
   private int editWidth;
-
   private String hintText;
   private RectF underbarRect;
   private Paint underbarRectPaint;
-  private int viewWidth;
-  private int viewHeight;
+  private int maxLength;
 
   public RightAlignedHintEdit(Context context) {
     super(context);
@@ -50,6 +50,8 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
       editWidth = (int) a.getDimension(R.styleable.RightAlignedHintEdit_editWidth, 0f);
       hintText = a.getString(R.styleable.RightAlignedHintEdit_android_hint);
       textColor = a.getColor(R.styleable.RightAlignedHintEdit_android_textColor, Color.WHITE);
+      maxValue = a.getFloat(R.styleable.RightAlignedHintEdit_maxValue, 100f);
+      maxLength = a.getInt(R.styleable.RightAlignedHintEdit_maxLength, 3);
     } finally {
       a.recycle();
     }
@@ -85,6 +87,12 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
     return hintText;
   }
 
+  public void setHintText(String hintText) {
+    this.hintText = hintText;
+    hint.setText(this.hintText);
+    invalidate();
+  }
+
   public int getTextColor() {
     return textColor;
   }
@@ -93,11 +101,6 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
     this.textColor = textColor;
     edit.setTextColor(textColor);
     hint.setTextColor(textColor);
-    invalidate();
-  }
-  public void setHintText(String hintText) {
-    this.hintText = hintText;
-    hint.setText(this.hintText);
     invalidate();
   }
 
@@ -111,7 +114,6 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
 
     inflate(getContext(), R.layout.right_aligned_hint_edit, this);
     edit = (EditText) findViewById(R.id.edit);
-    edit.setOnFocusChangeListener(this);
     hint = (TextView) findViewById(R.id.hint);
 
     underbarRect = new RectF();
@@ -122,6 +124,8 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
     setEditWidth(editWidth);
     setTextColor(textColor);
     edit.setWidth(editWidth);
+    edit.setOnFocusChangeListener(this);
+    edit.setFilters(new InputFilter[]{new ValueLengthFilter(maxValue, maxLength)});
   }
 
   @Override
@@ -129,12 +133,43 @@ public class RightAlignedHintEdit extends LinearLayout implements OnFocusChangeL
     if (v.getId() == R.id.edit) {
       if (hasFocus) {
         underbarRectPaint.setColor(focusColor);
-      }
-      else {
+      } else {
         underbarRectPaint.setColor(unfocusColor);
       }
 
       invalidate();
+    }
+  }
+
+  private class ValueLengthFilter implements InputFilter {
+
+    private final float maxValue;
+    private final int maxLength;
+
+    ValueLengthFilter(float maxValue, int maxLength) {
+      this.maxValue = maxValue;
+      this.maxLength = maxLength;
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dStart,
+        int dEnd) {
+      try {
+        // Remove the string out of destination that is to be replaced
+        String newVal = dest.toString().substring(0, dStart) + dest.toString()
+            .substring(dEnd, dest.toString().length());
+        newVal = newVal.substring(0, dStart) + source.toString() + newVal
+            .substring(dStart, newVal.length());
+        Integer input = Integer.parseInt(newVal);
+
+        if ((input <= maxValue) && (dest.length() <= maxLength)) {
+          return null;
+        }
+
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+      return "";
     }
   }
 }
